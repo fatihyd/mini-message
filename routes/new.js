53461a8aadd75c0d2fcedbc7869b6935fbc66a54
Fatih <yildizafatih@gmail.com>
2024-06-17 15:03:24 +0300

    index and new pages functionality done

--- routes/new.js
+++ routes/new.js
@@ -4,7 +4,17 @@ var router = express.Router();
 const messages = require("../messages");
 
 router.get("/", function (req, res, next) {
-  res.render("index", { messages });
+  res.render("form");
+});
+
+router.post("/", (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+
+  res.redirect("/");
 });
 
 module.exports = router;
